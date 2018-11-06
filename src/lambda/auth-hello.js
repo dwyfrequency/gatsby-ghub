024620a2d9bbed9bcb667b107555bad7e6396b4a
swyx <swyx@dontemail.me@>
2018-11-06 13:18:39 -0800

    auth

---
+++ src/lambda/auth-hello.js
@@ -0,0 +1,25 @@
+// from https://www.netlify.com/docs/functions/#identity-and-functions
+
+// Note that `netlify-lambda` only locally emulates Netlify Functions, while `netlify-identity-widget` interacts with a real Netlify Identity instance. This means that `netlify-lambda` doesn't support Netlify Functions + Netlify Identity integration.
+
+export function handler(event, context, callback) {
+  if (context.clientContext) {
+    const { identity, user } = context.clientContext
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        msg: 'auth-hello: ' + Math.round(Math.random() * 10),
+        identity,
+        user,
+      }),
+    })
+  } else {
+    // callback(new Error('error'))
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        hoo: 'bar',
+      }),
+    })
+  }
+}
