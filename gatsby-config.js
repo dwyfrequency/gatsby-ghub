5745eafbd2a8aaae3cb139fa28382044613c397d
swyx <swyx@dontemail.me@>
2018-11-06 13:57:18 -0800

    rename

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify Functions + Netlify Identity',
+    title: 'JAMstack Hackathon Starter',
     description: `Kick off your next, great Gatsby app.`,
     author: `@swyx`,
   },
