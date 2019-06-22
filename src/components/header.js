38221ecdb8b454b2385953787e6fa5619e28f290
dwyfrequency <jdwy215@gmail.com>
2019-06-22 19:14:07 -0400

    adding changes to navbar/header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from 'react'
+import { FaAlignRight } from 'react-icons/fa'
 import styles from '../css/navbar.module.css'
+import links from '../constants/links'
 
 // const NavList = styled.ul`
 //   display: flex;
@@ -13,69 +14,49 @@ import styles from '../css/navbar.module.css'
 //   }
 // `
 
-const Header = ({ siteTitle }) => (
-  <nav className={styles.navbar}>
-    <div className={styles.navCenter}>
-      <div className={styles.navHeader}>
-        {/* <img src={logo} alt="backroads logo" /> */}
-        <Link to={'/'} className={styles.brandLinks}>
-          <h2 className={styles.brand}>{siteTitle}</h2>
-        </Link>
+const Header = ({ siteTitle }) => {
+  const [isCollapsed, toggleCollapsed] = useState(false)
 
-        <button
-          type="button"
-          className={styles.logoBtn}
-          onClick={toggleCollapsedNav}
+  const toggleCollapsedNav = () => {
+    toggleCollapsed(isCollapsed => !isCollapsed)
+  }
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          {/* <img src={logo} alt="backroads logo" /> */}
+          <Link to={'/'} className={styles.brandLinks}>
+            <h2 className={styles.brand}>{siteTitle}</h2>
+          </Link>
+
+          <button
+            type="button"
+            className={styles.logoBtn}
+            onClick={toggleCollapsedNav}
+          >
+            <FaAlignRight className={styles.logoIcon} />
+          </button>
+        </div>
+        <ul
+          className={
+            isCollapsed
+              ? `${styles.navLinks} ${styles.showNav}`
+              : styles.navLinks
+          }
         >
-          <FaAlignRight className={styles.logoIcon} />
-        </button>
+          {links.map(({ path, text }) => (
+            <li key={text}>
+              <Link fade to={path}>
+                {text}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul
-        className={
-          isCollapsed ? `${styles.navLinks} ${styles.showNav}` : styles.navLinks
-        }
-      >
-        {links.map(({ path, text }) => (
-          <li key={text}>
-            <Link fade to={path}>
-              {text}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </nav>
-  // <div
-  //   style={{
-  //     background: `rebeccapurple`,
-  //     marginBottom: `1.45rem`,
-  //   }}
-  // >
-  //   <div
-  //     style={{
-  //       margin: `0 auto`,
-  //       maxWidth: 960,
-  //       padding: `1.45rem 1.0875rem`,
-  //     }}
-  //   >
-  //     <h1 style={{ margin: 0, display: 'inline-block' }}>
-  //       <Link
-  //         to="/"
-  //         style={{
-  //           color: `white`,
-  //           textDecoration: `none`,
-  //         }}
-  //       >
-  //         {siteTitle}
-  //       </Link>
-  //     </h1>
-  //     <NavList>
-  //       <Link>Dynamic</Link>
-  //       <Link>Static</Link>
-  //     </NavList>
-  //   </div>
-  // </div>
-)
+    </nav>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
---
+++ src/constants/links.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    path: '/',
+    text: 'home',
+  },
+  // {
+  //   path: '/about',
+  //   text: 'about',
+  // },
+  // {
+  //   path: '/markdown',
+  //   text: 'portfolio',
+  // },
+  // {
+  //   path: '/blog',
+  //   text: 'blog',
+  // },
+  {
+    path: '/app',
+    text: 'app',
+  },
+]
