3c4790a4e7d5656ed8a6dbc2a45de5ce6f4b6eae
dwyfrequency <jdwy215@gmail.com>
2019-06-22 18:51:30 -0400

    keep going

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,80 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import Image from '../components/image'
+import styled from 'styled-components'
+import styles from '../css/navbar.module.css'
+
+// const NavList = styled.ul`
+//   display: flex;
+//   justify-content: flex-end;
+//   a {
+//     color: white;
+//     text-decoration: none;
+//   }
+// `
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+  <nav className={styles.navbar}>
+    <div className={styles.navCenter}>
+      <div className={styles.navHeader}>
+        {/* <img src={logo} alt="backroads logo" /> */}
+        <Link to={'/'} className={styles.brandLinks}>
+          <h2 className={styles.brand}>{siteTitle}</h2>
         </Link>
-      </h1>
+
+        <button
+          type="button"
+          className={styles.logoBtn}
+          onClick={toggleCollapsedNav}
+        >
+          <FaAlignRight className={styles.logoIcon} />
+        </button>
+      </div>
+      <ul
+        className={
+          isCollapsed ? `${styles.navLinks} ${styles.showNav}` : styles.navLinks
+        }
+      >
+        {links.map(({ path, text }) => (
+          <li key={text}>
+            <Link fade to={path}>
+              {text}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
-  </div>
+  </nav>
+  // <div
+  //   style={{
+  //     background: `rebeccapurple`,
+  //     marginBottom: `1.45rem`,
+  //   }}
+  // >
+  //   <div
+  //     style={{
+  //       margin: `0 auto`,
+  //       maxWidth: 960,
+  //       padding: `1.45rem 1.0875rem`,
+  //     }}
+  //   >
+  //     <h1 style={{ margin: 0, display: 'inline-block' }}>
+  //       <Link
+  //         to="/"
+  //         style={{
+  //           color: `white`,
+  //           textDecoration: `none`,
+  //         }}
+  //       >
+  //         {siteTitle}
+  //       </Link>
+  //     </h1>
+  //     <NavList>
+  //       <Link>Dynamic</Link>
+  //       <Link>Static</Link>
+  //     </NavList>
+  //   </div>
+  // </div>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,44 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 import { initAuth } from '../app/services/auth'
 initAuth()
 
-class IndexPage extends React.Component {
-  render() {
-    const { loading, msg } = this.state
-    return (
-      <Layout>
-        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          <div>
-            <h2>Static Content Lives Here</h2>
-            <ul>
-              <li>
-                This site has statically generated marketing pages like this one
-                and <Link to="/page-2/">page 2.</Link>{' '}
-              </li>
-              <li>
-                It also has a dynamically generated clientside app guarded by
-                authentication:
-                <ul>
-                  <li>
-                    <Link to="/app/">
-                      <b>Go to App (with Netlify Identity)</b>
-                    </Link>{' '}
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <hr />
-            <br />
-          </div>
+export default () => {
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div>
+          <h2>Static Content Lives Here</h2>
+          <ul>
+            <li>
+              This site has statically generated marketing pages like this one
+              and <Link to="/page-2/">page 2.</Link>{' '}
+            </li>
+            <li>
+              It also has a dynamically generated clientside app guarded by
+              authentication:
+              <ul>
+                <li>
+                  <Link to="/app/">
+                    <b>Go to App (with Netlify Identity)</b>
+                  </Link>{' '}
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <hr />
+          <br />
         </div>
-      </Layout>
-    )
-  }
+      </div>
+    </Layout>
+  )
 }
-
-export default IndexPage
