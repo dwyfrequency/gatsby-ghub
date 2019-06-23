3df3d469effd854b3e82c28cc52728679c6399ed
dwyfrequency <jdwy215@gmail.com>
2019-06-23 10:59:03 -0400

    created null check for primary language

--- src/components/RepoListItem.js
+++ src/components/RepoListItem.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-const RepoListItem = ({ data }) => {
-  return <div>repo details</div>
+const RepoListItem = ({ repoDetails }) => {
+  const {
+    url,
+    name,
+    primaryLanguage,
+    stargazers: { totalCount },
+  } = repoDetails
+  console.log(primaryLanguage)
+  return (
+    <div>
+      repo details
+      <p>
+        <a href={url}>{name}</a>
+      </p>
+      <p>{primaryLanguage ? primaryLanguage.name : 'JavaScript'}</p>
+      <p>stars: {totalCount}</p>
+    </div>
+  )
 }
 
 export default RepoListItem
