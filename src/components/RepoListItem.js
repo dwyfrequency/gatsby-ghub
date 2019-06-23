da08eba9d3994e0dcbd864c2b9297489f30fafc8
dwyfrequency <jdwy215@gmail.com>
2019-06-23 12:11:44 -0400

    mit
    adding some styling

--- src/components/RepoListItem.js
+++ src/components/RepoListItem.js
@@ -1,4 +1,35 @@
 import React from 'react'
+import { MdStar } from 'react-icons/md'
+import styled from 'styled-components'
+
+const Card = styled.div`
+  width: 80vh;
+  height: 100px;
+  background-color: rgb(34, 17, 51);
+  border-radius: 4px;
+  padding: 1rem;
+  margin: 1rem auto;
+  color: rgb(63, 169, 245);
+  h4 {
+    padding: 0;
+  }
+  a {
+    text-decoration: none;
+    color: #fff;
+  }
+  ul {
+    display: flex;
+    margin-left: 2px;
+  }
+  li {
+    margin: 0 1rem;
+    padding: 3px;
+    border-radius: 4px;
+    list-style: none;
+    background: rebeccapurple;
+    color: #fff;
+  }
+`
 
 const RepoListItem = ({ repoDetails }) => {
   const {
@@ -7,16 +38,19 @@ const RepoListItem = ({ repoDetails }) => {
     primaryLanguage,
     stargazers: { totalCount },
   } = repoDetails
-  console.log(primaryLanguage)
   return (
-    <div>
-      repo details
-      <p>
+    <Card>
+      <h4>
         <a href={url}>{name}</a>
-      </p>
-      <p>{primaryLanguage ? primaryLanguage.name : 'JavaScript'}</p>
-      <p>stars: {totalCount}</p>
-    </div>
+      </h4>
+      <ul>
+        <li>{primaryLanguage ? primaryLanguage.name : 'JavaScript'}</li>
+        <li>
+          <MdStar css={{ verticalAlign: 'sub', color: 'gold' }} size={20} />{' '}
+          {totalCount}
+        </li>
+      </ul>
+    </Card>
   )
 }
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Layout from './layout'
+import { FaGithub } from 'react-icons/fa'
 import { graphql, useStaticQuery } from 'gatsby'
 import RepoList from './RepoList'
+import styled from 'styled-components'
 
 const query = graphql`
   query {
@@ -82,6 +84,17 @@ const query = graphql`
   }
 `
 
+const Showcase = styled.div`
+  /* margin: 0 auto; */
+  text-align: center;
+  h2 {
+    margin-bottom: 4px;
+  }
+  li {
+    list-style: none;
+  }
+`
+
 const Resume = () => {
   const data = useStaticQuery(query)
   const {
@@ -99,19 +112,40 @@ const Resume = () => {
   console.log(avatarUrl)
   return (
     <>
-      <h2>Resume</h2>
-      {/* <img src={avatarUrl} alt="avatar image" /> */}
-      <h2>{name}</h2>
-      <h3>{login}</h3>
-      <p>{bio || 'Software Engineer'}</p>
-      <p>total commits: {totalCommitContributions}</p>
+      <Showcase>
+        <h2>Resume</h2>
+        <img
+          src={avatarUrl}
+          alt="avatar image"
+          style={{ width: 350, height: 350, borderRadius: 6 }}
+        />
+        <h2>{name}</h2>
+        <h3>
+          <FaGithub
+            size={25}
+            css={{
+              marginRight: '0.2rem',
+              verticalAlign: 'sub',
+            }}
+          />
+          {login}
+        </h3>
+        <ul>
+          <li>Bio: {bio || 'Software Engineer'}</li>
+          <li>Total Commits: {totalCommitContributions}</li>
+        </ul>
+      </Showcase>
+
       <hr />
       {location || 'New York'}
       {email || 'userhasnoemail@me.com'}
       <hr />
       <RepoList title="Contributed to" repos={repositoriesContributedTo} />
       <hr />
-      <h3>Personal Repos</h3>
+      <RepoList
+        title="Personal Repos"
+        repos={pinnedItems.edges.length ? pinnedItems : repositories}
+      />
     </>
   )
 }
