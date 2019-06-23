fe2a83bed2a6d193555b795b22161fe7d592d463
dwyfrequency <jdwy215@gmail.com>
2019-06-23 15:02:44 -0400

    personal and contributed repos rendering

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,3 +8,129 @@
 
 // note we do use https://www.gatsbyjs.org/packages/gatsby-plugin-create-client-paths/
 // for more info read https://www.gatsbyjs.org/docs/authentication-tutorial/#creating-client-only-routes
+
+const path = require(`path`)
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions // const response = await graphql(`
+  //   query {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
+  // const response = await graphql(`
+  //   {
+  //     github {
+  //       organization(login: "FullstackAcademy") {
+  //         membersWithRole(first: 1) {
+  //           edges {
+  //             node {
+  //               login
+  //             }
+  //             cursor
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+  // const response = await graphql(`
+  //   {
+  //     github {
+  //       user(login: "dwyfrequency") {
+  //         login
+  //       }
+  //     }
+  //   }
+  // `)
+
+  // console.log('>>>>>>>>>>', response)
+  // console.log('>>>>>>>>>>', response.data.github.user.login)
+
+  const { data } = await graphql(`
+    query {
+      github {
+        organization(login: "FullstackAcademy") {
+          team(slug: "1904-fsa-ny") {
+            members(first: 1) {
+              edges {
+                node {
+                  avatarUrl
+                  bio
+                  location
+                  email
+                  login
+                  name
+                  websiteUrl
+                  contributionsCollection {
+                    totalCommitContributions
+                  }
+                  repositories(last: 5) {
+                    edges {
+                      node {
+                        id
+                        url
+                        name
+                        primaryLanguage {
+                          name
+                          color
+                        }
+                        updatedAt
+                        owner {
+                          login
+                        }
+                        stargazers {
+                          totalCount
+                        }
+                        forkCount
+                      }
+                    }
+                  }
+                  repositoriesContributedTo(last: 5) {
+                    totalCount
+                    edges {
+                      node {
+                        id
+                        url
+                        name
+                        primaryLanguage {
+                          name
+                          color
+                        }
+                        updatedAt
+                        owner {
+                          login
+                        }
+                        stargazers {
+                          totalCount
+                        }
+                        forkCount
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log('>>>>>>>>>>', data)
+  data.github.organization.team.members.edges.forEach(({ node }) =>
+    console.log('>>>>>>>>>>', node.login, node.name)
+  )
+
+  data.github.organization.team.members.edges.forEach(({ node }) => {
+    createPage({
+      path: `resumes/${node.login}`,
+      component: path.resolve(`./src/templates/UserPageGh.js`),
+      context: {
+        resumeDetails: node,
+      },
+    })
+  })
+}
--- src/templates/UserPageGh.js
+++ src/templates/UserPageGh.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import Resume2 from '../components/Resume2'
 
-const UserPageGh = () => {
+const UserPageGh = ({ pathContext: { resumeDetails } }) => {
+  console.log(resumeDetails)
   return (
     <Layout>
       <div>UserPageTemplate</div>
+      <Resume2 resumeDetails={resumeDetails} />
     </Layout>
   )
 }
