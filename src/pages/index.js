6994f65aa8249a8b449a344559c57c36221a00a1
dwyfrequency <jdwy215@gmail.com>
2019-06-28 23:01:41 -0400

    adding gatsby image back in and adding back full amount of resumes for query

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Img from '../components/image'
 
 export default () => {
   return (
@@ -33,6 +34,7 @@ export default () => {
             </li>
           </ul>
           <hr />
+          <Img />
           <br />
         </div>
       </div>
